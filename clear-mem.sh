3c7962e91627c7f588da87b5df029da1f1ac80fa
Aziz <abdul.aziz@muf.co.id>
2020-08-05 09:40:59 +0700

    Formatting comment.

--- clear-mem.sh
+++ clear-mem.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#Calculates the current percentage of free RAM available in system
+# Calculates the current percentage of free RAM available in system
 _ram_avl=`free | grep Mem | awk '{print $4/$2 * 100.0}' | awk -F'.' '{print $1}'`
-#If free percentage is less than equal to 15% of total RAM trigger the cleanup
-#echo $_ram_avl
+# If free percentage is less than equal to 15% of total RAM trigger the cleanup
+# echo $_ram_avl
 if [ $_ram_avl -le 60 ]; then
 echo "RAM available in system is below 60%. Clearing cache and freeing up memory :";
 echo "================================================================================"
@@ -10,8 +10,8 @@ free && sync; echo 3 > /proc/sys/vm/drop_caches
 echo "================================================================================"
 free
 echo "================================================================================"
-#sudo sysctl vm.drop_caches=1
-#echo
+# sudo sysctl vm.drop_caches=1
+# echo
 else
 echo "No need action."
 fi;
